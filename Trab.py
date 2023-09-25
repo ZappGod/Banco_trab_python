919b12ebb4a01e60e0360bc1f5020b1e26baa1ea
ZappGod <GABRIELLSZAPP@gmail.com>
2023-09-25 20:27:39 -0300

    atualização geral

--- Trab.py
+++ Trab.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#lê arquivo
+#Lê arquivo
 df = pd.read_csv("dados.csv",encoding="latin-1")
 
 #Os maiores valores e seus respectivos anos
@@ -12,7 +12,9 @@ vm_max = df["valor"].max()
 dm_min = df.loc[df["valor"].idxmin()]["data"]
 vm_min = df["valor"].min()
 
+#Grafico do maior e menor de todos os anos
 plt.bar(dm_max,vm_max)
 plt.bar(dm_min,vm_min)
+plt.title("Maior e menor valor dos anos")
+plt.xlabel("Ano")
 plt.show()
-
