b3941334f06e1e0330af02ae066709c2b39f0bc9
Hoff-Otavio <otavio_hoff@oulook.com>
2023-09-26 21:40:10 -0300

    InProd

---
+++ request_test/API/main.py
@@ -0,0 +1,6 @@
+import requests
+import menu
+
+
+
+print("In prod")
---
+++ request_test/API/menu.py
@@ -0,0 +1,23 @@
+def menu():
+    print('''
+            Menu:
+          
+        [1] plotar gráficos
+        [2] Documentação.
+        [3] Requisitos.
+        [4] Sair.
+             
+          ''')
+    r = input()
+
+    if r == 1:
+        print('''
+        Gráfico 
+
+
+
+''')
+    
+
+
+
